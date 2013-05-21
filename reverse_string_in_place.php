78fb972ac6644d406b1a07cc28f4d5bbb7e0832a
Peter Keane <pkeane@mail.utexas.edu>
2013-05-21 17:12:03 -0500

    in place

---
+++ reverse_string_in_place.php
@@ -0,0 +1,40 @@
+<?php
+
+$tests = array(
+  'once upon a time there was a red frog',
+  'a',
+  'an even string',
+  'an odd string',
+  'dghfgjdfgjfvdfms fjfgjjfgjisfg fogoitgoittg fgsrgoirgjir'
+);
+
+
+function revstr($str) {
+  $new_str = array();
+  foreach (str_split($str) as $ch) {
+    array_unshift($new_str,$ch);
+  }
+  return join('',$new_str);
+}
+
+function firstShallBeLast($str,$index=0) {
+  $len = strlen($str);
+  if ($index+1 > $len/2) {
+    return $str;
+  }
+  $first = $str[$index];
+  $last = $str[$len-($index+1)];
+  $str[$index] = $last;
+  $str[$len-($index+1)] = $first;
+  $index++;
+  return firstShallBeLast($str,$index);
+}
+
+foreach ($tests as $s) {
+  if (firstShallBeLast($s) == revstr($s)) {
+    print "OK! $s\n";
+  } else {
+    print "FAIL! $s\n";
+  }
+}
+
